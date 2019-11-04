60d801f70762eb60e55d726dd8b4a1dfd2010343
Valien R <valien.r@gmail.com>
2019-11-04 12:26:36 +0300

    add database sample

---
+++ database_sample/make_main_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE public.chat_ids (
+	chat_id int4 NULL,
+	"name" varchar NULL
+);
+
+
+/* name of the table - can be different and you need to specify it in Config Vars as CHANNEL_NAME */
+CREATE TABLE public.detektivo (
+	subscribers int4 NULL,
+	"views" int4 NULL,
+	datetime timestamp NULL
+);
+
+CREATE TABLE public.yt_query_log (
+	chat_id varchar(10) NULL,
+	datetime timestamptz NULL
+);
+
+
+commit
