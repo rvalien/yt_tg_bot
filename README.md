ab5d67f8743470b2c84d74058ecdc2f333bfa6ff
valien <valien.r@gmail.com>
2021-06-18 12:50:03 +0300

    remove all optional code

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# telegram bot for your youtube channel
+# Telegram bot for your youtube channel
 1. the bot creates log
 2. the bot shows subscribers and view statistic info for selected youtube channel.
 3. once every n seconds the bot check the change in the number of subscribers and sends a notification to the designated contacts if the numbers have changed
-4. additionally the bot informs about the weather [openweathermap](https://openweathermap.org/)
-5. the bot show rest of the Internet traffic (only for my provider)
 
-build on [heroku](https://www.heroku.com/) and use posgressql
+build on [heroku](https://www.heroku.com/) and use postgres
 
 ![day stat](samples/day.png)
 
@@ -13,9 +11,7 @@ build on [heroku](https://www.heroku.com/) and use posgressql
 
 ![month stat](samples/month.png)
 
-don't forget to [create tables](samples/make_main_tables.sql)
-
-and specify your config vars on Heroku
+don't forget to [create tables](samples/make_main_tables.sql) and specify your config vars on Heroku
 
 |**variable name**|type|description|
 |:---|:---:|:---|
@@ -23,7 +19,6 @@ and specify your config vars on Heroku
 `DATABASE_URL`|str|connection string for your database|
 `PROJECT_NAME`|str|just name of your project. use to make heroku web hook|
 `TELEGRAM_TOKEN`|str|create your bot and get the token [here](https://tlgrm.ru/docs/bots)|
-`WEATHER_TOKEN`|str|get you own token [here](https://home.openweathermap.org/users/sign_up)|
 `YOUTUBE_TOKEN`|str|your token [more info](https://developers.google.com/youtube/registering_an_application)|
 `DELAY`|int|sleep time (seconds)|
 
@@ -37,21 +32,18 @@ os.environ['CHANNEL_NAME'] = 'name of the channel and database you create'
 os.environ['DATABASE_URL'] = 'con. string'
 os.environ['PROJECT_NAME'] = 'bot name'
 os.environ['TELEGRAM_TOKEN'] = '<your token>'
-os.environ['WEATHER_TOKEN'] = '<your token>'
 os.environ['YOUTUBE_TOKEN'] = '<your token>'
 os.environ['DELAY'] = 900 # sleep time
 ```
 
 ***
 
-# telegram bot для вашего youtube канала: 
+# Telegram bot для вашего youtube канала: 
 1. пишет логи
-2. возврашать по запросу количетво просмотров и подписчиков канала на youtube.com
+2. возвращать по запросу количество просмотров и подписчиков канала на youtube.com
 3. раз в n секунд проверять изменение количества подписчиков и отправляет уведомление обозначенным контактам если цифры изменились
-4. по запросу возвращать погоду [openweathermap](https://openweathermap.org/)
-5. показывает информацию о остатке интернет трафика (актуально только для моего провайдера)
 
-настроен на работу с сервисом [heroku](https://www.heroku.com/)  и базой данных posgressql.
+Настроен на работу с сервисом [heroku](https://www.heroku.com/) и базой данных posgressql.
 
 ![day stat](samples/day.png)
 
@@ -59,10 +51,7 @@ os.environ['DELAY'] = 900 # sleep time
 
 ![month stat](samples/month.png)
 
-не забудь [создать нужные таблицы](samples/make_main_tables.sql)
-
-
-и прописать переменные окружения в Heroku
+Не забудь [создать нужные таблицы](samples/make_main_tables.sql) и прописать переменные окружения в Heroku
 
 
 |**variable name**|type|description|
@@ -71,12 +60,11 @@ os.environ['DELAY'] = 900 # sleep time
 `DATABASE_URL`|str|connection string for your database|
 `PROJECT_NAME`|str|just name of your project. use to make heroku web hook|
 `TELEGRAM_TOKEN`|str|create your bot and get the token [here](https://tlgrm.ru/docs/bots)|
-`WEATHER_TOKEN`|str|get you own token [here](https://home.openweathermap.org/users/sign_up)|
 `YOUTUBE_TOKEN`|str|your token [more info](https://developers.google.com/youtube/registering_an_application)|
 `DELAY`|int|sleep time (seconds)|
 
 
-для локального испольнения, создай файл `config.py` с переменными окружения
+Для локального исполнения, создай файл `config.py` с переменными окружения
 
 ```python
 import os
@@ -85,7 +73,6 @@ os.environ['CHANNEL_NAME'] = 'name of the channel and database you create'
 os.environ['DATABASE_URL'] = 'con. string'
 os.environ['PROJECT_NAME'] = 'bot name'
 os.environ['TELEGRAM_TOKEN'] = '<your token>'
-os.environ['WEATHER_TOKEN'] = '<your token>'
 os.environ['YOUTUBE_TOKEN'] = '<your token>'
 os.environ['DELAY'] = 900 # sleep time
 ```
--- bot.py
+++ bot.py
@@ -9,8 +9,7 @@ from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.types import KeyboardButton
 from aiogram.dispatcher import Dispatcher
-from utils import get_weather, get_ststel_data, print_ststel_info, free_time
-# from youtube_utils import get_data_day, get_data_week, get_data_month, make_picture, prepare_text, get_yt, write_data
+from youtube_utils import get_data_day, get_data_week, get_data_month, make_picture, prepare_text, get_yt, write_data
 
 print("init bot")
 
@@ -23,7 +22,6 @@ if sys.platform == "win32":
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 telegram_token = os.environ["TELEGRAM_TOKEN"]
 youtube_token = os.environ["YOUTUBE_TOKEN"]
-weather_token = os.environ["WEATHER_TOKEN"]
 database = os.environ["DATABASE_URL"]
 stat_table = os.environ["CHANNEL_NAME"]
 delay = int(os.environ["DELAY"])
@@ -64,9 +62,7 @@ conn.close()
 print("done")
 
 markup = types.ReplyKeyboardMarkup()
-# markup.row(KeyboardButton("day 📈"), KeyboardButton("week 📈"), KeyboardButton("month 📅"))
-markup.row(KeyboardButton("🌤 weather 🌧"), KeyboardButton("/time"))
-markup.row(KeyboardButton("📱 internet 🌐"), KeyboardButton("📱 bill 🌐"))
+markup.row(KeyboardButton("day 📈"), KeyboardButton("week 📈"), KeyboardButton("month 📅"))
 
 
 @dp.message_handler(commands=["start"])
@@ -77,86 +73,40 @@ async def send_welcome(message: types.Message):
     )
 
 
-# @dp.message_handler(regexp="day..")
-# async def worker(message):
-#     media = types.MediaGroup()
-#     statistic_df = get_data_day(database, n_days=2)
-#     raw = get_yt(youtube_token)
-#     text = prepare_text(database, raw)
-#     make_picture(statistic_df.diff(-1).apply(abs))
-#     media.attach_photo(types.InputFile("hour.png"), text)
-#     await types.ChatActions.upload_photo()
-#     await message.reply_media_group(media=media)
-#
-#
-# @dp.message_handler(regexp="week..")
-# async def worker(message):
-#     media = types.MediaGroup()
-#     statistic_df = get_data_week(database)
-#     raw = get_yt(youtube_token)
-#     text = prepare_text(database, raw)
-#     make_picture(statistic_df)
-#     media.attach_photo(types.InputFile("day of week.png"), text)
-#     await types.ChatActions.upload_photo()
-#     await message.reply_media_group(media=media)
-#
-#
-# @dp.message_handler(regexp="month..")
-# async def worker(message):
-#     media = types.MediaGroup()
-#     statistic_df = get_data_month(database)
-#     raw = get_yt(youtube_token)
-#     text = prepare_text(database, raw)
-#     make_picture(statistic_df)
-#     media.attach_photo(types.InputFile("day of month.png"), text)
-#     await types.ChatActions.upload_photo()
-#     await message.reply_media_group(media=media)
-#
-
-@dp.message_handler(regexp="..weather..")
+@dp.message_handler(regexp="day..")
 async def worker(message):
-    await types.ChatActions.typing(1)
-    await message.reply(get_weather(weather_token))
+    media = types.MediaGroup()
+    statistic_df = get_data_day(database, n_days=2)
+    raw = get_yt(youtube_token)
+    text = prepare_text(database, raw)
+    make_picture(statistic_df.diff(-1).apply(abs))
+    media.attach_photo(types.InputFile("hour.png"), text)
+    await types.ChatActions.upload_photo()
+    await message.reply_media_group(media=media)
 
 
-@dp.message_handler(commands="time")
+@dp.message_handler(regexp="week..")
 async def worker(message):
-    await types.ChatActions.typing(1)
-    await message.reply(free_time(message, CLIENT))
+    media = types.MediaGroup()
+    statistic_df = get_data_week(database)
+    raw = get_yt(youtube_token)
+    text = prepare_text(database, raw)
+    make_picture(statistic_df)
+    media.attach_photo(types.InputFile("day of week.png"), text)
+    await types.ChatActions.upload_photo()
+    await message.reply_media_group(media=media)
 
 
-@dp.message_handler(regexp="..internet..")
+@dp.message_handler(regexp="month..")
 async def worker(message):
-    await types.ChatActions.typing(2)
-    conn = psycopg2.connect(database)
-    cursor = conn.cursor()
-    cursor.execute(f"select phone, password from users where chat_id = {message['from']['id']}")
-    res = cursor.fetchone()
-    await message.reply(str(print_ststel_info(get_ststel_data(*res))))
-
-
-@dp.message_handler(regexp="..bill..")
-async def worker(message):
-    await types.ChatActions.typing(2)
-    conn = psycopg2.connect(database)
-    cursor = conn.cursor()
-    cursor.execute("select phone, password, name from users")
-    res = cursor.fetchall()
-
-    def get_all_mobile_bills(all_users):
-        result = dict()
-        for user in all_users:
-            result[user[2]] = get_ststel_data(user[0], user[1])
-        return result
-
-    def prepare_response_text(data):
-        temp_list = list()
-        for key in data.keys():
-            temp = f'{key}: {data[key].get("effectiveBalance") if data[key].get("effectiveBalance") else data[key].get("balance")}'
-            temp_list.append(temp)
-        return "\n".join(temp_list)
-
-    await message.reply(prepare_response_text(get_all_mobile_bills(res)))
+    media = types.MediaGroup()
+    statistic_df = get_data_month(database)
+    raw = get_yt(youtube_token)
+    text = prepare_text(database, raw)
+    make_picture(statistic_df)
+    media.attach_photo(types.InputFile("day of month.png"), text)
+    await types.ChatActions.upload_photo()
+    await message.reply_media_group(media=media)
 
 
 @dp.message_handler(regexp="myid")
@@ -164,58 +114,42 @@ async def worker(message):
     await types.ChatActions.typing(2)
     await message.reply(message.from_user)
 
-#
-# async def auto_yt_check(send=False):
-#     """
-#     check youtube subscribers and sand message every <daley> seconds if new counts not the same as last count
-#     add check result to log data
-#     disable sending if it's sleep time
-#     :param send: sanding massage is active
-#     :return: None
-#     """
-#     response = get_yt(youtube_token)
-#     current_subs = int(response.get("items")[0].get("statistics").get("subscriberCount"))
-#     connection = psycopg2.connect(database)
-#     cursor = connection.cursor()
-#     with open("sql_queries/max_db_data.sql") as sql_file:
-#         query = sql_file.read()
-#
-#     cursor.execute(query)
-#     db_hour, db_views, db_subs = map(int, cursor.fetchone())
-#     conn.close()
-#     write_data(database, response)
-#     if send:
-#         if night_to < datetime.datetime.now().time() < night_from:
-#             print("x" * 10)
-#             print(f"database: hour: {db_hour}, subscribers: {db_subs}")
-#             print(f"cur time: hour: {datetime.datetime.now().hour}, subscribers: {current_subs}")
-#             if db_subs == current_subs or datetime.datetime.now().hour == db_hour:
-#                 print("pass")
-#                 pass
-#             else:
-#                 print("work")
-#                 for chat_id in chat_ids:
-#                     await bot.send_message(
-#                         chat_id,
-#                         # два пробела тут, что бы выравнять по строки --> <--
-#                         str(f"Изменеие в количестве подписчиков.\nбыло:  {db_subs}\nстало: {current_subs}"),
-#                     )
 
+async def auto_yt_check(send=False):
+    """
+    check youtube subscribers and sand message every <daley> seconds if new counts not the same as last count
+    add check result to log data
+    disable sending if it's sleep time
+    :param send: sanding massage is active
+    :return: None
+    """
+    response = get_yt(youtube_token)
+    current_subs = int(response.get("items")[0].get("statistics").get("subscriberCount"))
+    connection = psycopg2.connect(database)
+    cursor = connection.cursor()
+    with open("sql_queries/max_db_data.sql") as sql_file:
+        query = sql_file.read()
 
-# async def count_db_rows():
-#     # TODO убрать после переходна на новую таблицу
-#     """
-#     временная функция для отслеживания количества записей в базе данных. ограничение на бесплатном тарифе 10000 строк
-#     нужно, пока не переехал на другую бд.
-#     :return:
-#     """
-#     conn = psycopg2.connect(database)
-#     cursor = conn.cursor()
-#     cursor.execute(f"select count(*) from {stat_table}")
-#     count_rows = cursor.fetchall()[0][0]
-#     if count_rows > 9500:
-#         for chat_id in ["464620721"]:
-#             await bot.send_message(chat_id=chat_id, text=str(f"Делай резервную копию базы. Строк сейчас: {count_rows}"))
+    cursor.execute(query)
+    db_hour, db_views, db_subs = map(int, cursor.fetchone())
+    conn.close()
+    write_data(database, response)
+    if send:
+        if night_to < datetime.datetime.now().time() < night_from:
+            print("x" * 10)
+            print(f"database: hour: {db_hour}, subscribers: {db_subs}")
+            print(f"cur time: hour: {datetime.datetime.now().hour}, subscribers: {current_subs}")
+            if db_subs == current_subs or datetime.datetime.now().hour == db_hour:
+                print("pass")
+                pass
+            else:
+                print("work")
+                for chat_id in chat_ids:
+                    await bot.send_message(
+                        chat_id,
+                        # два пробела тут, что бы выравнять по строки --> <--
+                        str(f"Изменеие в количестве подписчиков.\nбыло:  {db_subs}\nстало: {current_subs}"),
+                    )
 
 
 def repeat(coro, loop):
@@ -225,6 +159,5 @@ def repeat(coro, loop):
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    # loop.call_later(delay, repeat, auto_yt_check, loop)
-    # loop.call_later(delay, repeat, count_db_rows, loop)
+    loop.call_later(delay, repeat, auto_yt_check, loop)
     asyncio.run(executor.start_polling(dp, loop=loop))
